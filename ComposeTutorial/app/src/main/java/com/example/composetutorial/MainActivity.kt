4867c70f12af238c773796e5c9526d437a02fed9
Travis <gtthh123@gmail.com>
2024-01-25 17:14:09 +0800

    Update

--- ComposeTutorial/app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ ComposeTutorial/app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -4,17 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetutorial.ui.theme.ComposeTutorialTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material3.Button
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,9 +39,13 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun myFunc(modifier: Modifier=Modifier,names: List<String> = listOf("World", "Compose")){
     Surface(modifier=modifier,color=MaterialTheme.colorScheme.background) {
-Column {
+Column (modifier = modifier.padding(vertical = 4.dp)){
     for (name in names){
-        Greeting(name = name)
+
+            Greeting(name = name)
+
+
+
     }
 }
     }
@@ -41,18 +53,66 @@ Column {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary, modifier = modifier.padding(vertical=4.dp, horizontal = 8.dp)) {
-        Column() {
-            Text(text = "Hello ")
-            Text(text = "$name")
+    var expanded = remember{ mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier=Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom=extraPadding)) {
+                Text(text = "Hello ")
+                Text(text = name)
+            }
+            ElevatedButton(onClick = { expanded.value=!expanded.value }) {
+                if (expanded.value){
+                    Text("Show less")
+                }
+                else {
+
+                    Text("Show more")
+                }
+                }
+            }
         }
+
     }
-}
+
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     ComposeTutorialTheme {
         myFunc()
+    }
+}
+
+@Composable
+fun OnboardingScreen(modifier: Modifier = Modifier) {
+    // TODO: This state should be hoisted
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier = Modifier.padding(vertical = 24.dp),
+            onClick = { shouldShowOnboarding = false }
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    ComposeTutorialTheme {
+        OnboardingScreen()
     }
 }
